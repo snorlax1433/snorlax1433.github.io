a3083e90f88d242a01c7110f6588aa48abffebee
snorlax1433 <120232818+snorlax1433@users.noreply.github.com>
2022-12-09 19:46:30 -0800

    Add files via upload
    Signed-off-by: snorlax1433 <120232818+snorlax1433@users.noreply.github.com>

---
+++ assets/scripter.js
@@ -0,0 +1,325 @@
+/** ------------------------------------START of PREVENTIVE MAINTENANCE SERVER -------------------------------------------->**/
+$('#elementId').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName').prop('disabled',false);
+            
+        }else{
+            $('#FullName').prop('disabled',true);
+             document.getElementById('FullName').value = "";
+        }
+       
+});
+
+$('#elementId1').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName1').prop('disabled',false);
+             
+        }else{
+            $('#FullName1').prop('disabled',true);
+            document.getElementById('FullName1').value = "";
+        }
+       
+});
+$('#elementId2').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName2').prop('disabled',false);
+        }else{
+            $('#FullName2').prop('disabled',true);
+              document.getElementById('FullName2').value = "";
+        }
+       
+});
+$('#elementId3').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName3').prop('disabled',false);
+             
+        }else{
+            $('#FullName3').prop('disabled',true);
+            document.getElementById('FullName3').value = "";
+        }
+       
+});
+$('#elementId4').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName4').prop('disabled',false);
+             
+        }else{
+            $('#FullName4').prop('disabled',true);
+            document.getElementById('FullName4').value = "";
+        }
+       
+});
+$('#elementId5').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName5').prop('disabled',false);
+             
+        }else{
+            $('#FullName5').prop('disabled',true);
+            document.getElementById('FullName5').value = "";
+        }
+       
+});
+$('#elementId6').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName6').prop('disabled',false);
+             
+        }else{
+            $('#FullName6').prop('disabled',true);
+            document.getElementById('FullName6').value = "";
+        }
+       
+});
+$('#elementId7').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName7').prop('disabled',false);
+            
+        }else{
+            $('#FullName7').prop('disabled',true);
+             document.getElementById('FullName7').value = "";
+        }
+       
+});
+$('#elementId8').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName8').prop('disabled',false);
+           
+        }else{
+            $('#FullName8').prop('disabled',true);
+              document.getElementById('FullName8').value = "";
+        }
+       
+});
+$('#elementId9').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName9').prop('disabled',false);
+             
+        }else{
+            $('#FullName9').prop('disabled',true);
+            document.getElementById('FullName9').value = "";
+        }
+       
+});
+$('#elementId10').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName10').prop('disabled',false);
+          
+        }else{
+            $('#FullName10').prop('disabled',true);
+               document.getElementById('FullName10').value = "";
+        }
+       
+});
+$('#elementId11').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName11').prop('disabled',false);
+            
+        }else{
+            $('#FullName11').prop('disabled',true);
+             document.getElementById('FullName11').value = "";
+        }
+       
+});
+$('#elementId12').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName12').prop('disabled',false);
+          
+        }else{
+            $('#FullName12').prop('disabled',true);
+               document.getElementById('FullName12').value = "";
+        }
+       
+});
+$('#elementId13').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName13').prop('disabled',false);
+             
+        }else{
+            $('#FullName13').prop('disabled',true);
+            document.getElementById('FullName13').value = "";
+        }
+       
+});
+$('#elementId14').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName14').prop('disabled',false);
+           
+        }else{
+            $('#FullName14').prop('disabled',true);
+              document.getElementById('FullName14').value = "";
+        }
+       
+});
+$('#elementId15').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName15').prop('disabled',false);
+            
+        }else{
+            $('#FullName15').prop('disabled',true);
+             document.getElementById('FullName15').value = "";
+        }
+       
+});
+$('#elementId16').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName16').prop('disabled',false);
+            
+        }else{
+            $('#FullName16').prop('disabled',true);
+             document.getElementById('FullName16').value = "";
+        }
+       
+});
+$('#elementId17').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName17').prop('disabled',false);
+           
+        }else{
+            $('#FullName17').prop('disabled',true);
+              document.getElementById('FullName17').value = "";
+        }
+       
+});
+$('#elementId18').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName18').prop('disabled',false);
+           
+        }else{
+            $('#FullName18').prop('disabled',true);
+              document.getElementById('FullName18').value = "";
+        }
+       
+});
+$('#elementId19').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName19').prop('disabled',false);
+        
+        }else{
+            $('#FullName19').prop('disabled',true);
+                 document.getElementById('FullName19').value = "";
+        }
+       
+});
+
+/** ------------------------------------END of PREVENTIVE MAINTENANCE SERVER -------------------------------------------->**/
+
+/** ------------------------------------START of AIRCON SERVICE -------------------------------------------->**/
+$('#elementId20').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName20').prop('disabled',false);
+            
+        }else{
+            $('#FullName20').prop('disabled',true);
+             document.getElementById('FullName20').value = "";
+        }
+       
+});
+$('#elementId21').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName21').prop('disabled',false);
+            
+        }else{
+            $('#FullName21').prop('disabled',true);
+             document.getElementById('FullName21').value = "";
+        }
+       
+});
+$('#elementId22').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName22').prop('disabled',false);
+            
+        }else{
+            $('#FullName22').prop('disabled',true);
+             document.getElementById('FullName22').value = "";
+        }
+       
+});
+$('#elementId23').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName23').prop('disabled',false);
+            
+        }else{
+            $('#FullName23').prop('disabled',true);
+             document.getElementById('FullName23').value = "";
+        }
+       
+});
+$('#elementId24').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName24').prop('disabled',false);
+            
+        }else{
+            $('#FullName24').prop('disabled',true);
+             document.getElementById('FullName24').value = "";
+        }
+       
+});
+$('#elementId25').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName25').prop('disabled',false);
+            
+        }else{
+            $('#FullName25').prop('disabled',true);
+             document.getElementById('FullName25').value = "";
+        }
+       
+});
+$('#elementId26').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName26').prop('disabled',false);
+            
+        }else{
+            $('#FullName26').prop('disabled',true);
+             document.getElementById('FullName26').value = "";
+        }
+       
+});
+$('#elementId27').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName27').prop('disabled',false);
+            
+        }else{
+            $('#FullName27').prop('disabled',true);
+             document.getElementById('FullName27').value = "";
+        }
+       
+});
+$('#elementId28').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName28').prop('disabled',false);
+            
+        }else{
+            $('#FullName28').prop('disabled',true);
+             document.getElementById('FullName28').value = "";
+        }
+       
+});
+$('#elementId29').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName29').prop('disabled',false);
+            
+        }else{
+            $('#FullName29').prop('disabled',true);
+             document.getElementById('FullName29').value = "";
+        }
+       
+});
+$('#elementId30').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName30').prop('disabled',false);
+            
+        }else{
+            $('#FullName30').prop('disabled',true);
+             document.getElementById('FullName30').value = "";
+        }
+       
+});
+$('#elementId31').click(function(){
+        if($(this).is(':checked')){
+             $('#FullName31').prop('disabled',false);
+            
+        }else{
+            $('#FullName31').prop('disabled',true);
+             document.getElementById('FullName31').value = "";
+        }
+       
+});
